0342aeff65abaaadff3d4ad690d64360a21235c9
Nick Whiteley <102297051+nwhite34@users.noreply.github.com>
2023-05-14 22:37:11 +1000

    Updated Formatting ul li list
    Changes to the ul li list for formatting and preventing user from clicking button more than once.

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const likeButton = document.getElementById('like-button')
 const likeCount = document.getElementById('like-count')
 
 publishButtonEl.addEventListener("click", function () {
-  let inputValue = "To: " + "\n" + inputToEl.value + ", " + "\n" + inputFieldEl.value + "\n\nFrom: " + inputFromEl.value;
+  let inputValue = "To:" + inputToEl.value + "\n" + "\n" + inputFieldEl.value +   "\n" + "\n\nFrom:" + inputFromEl.value;
 
   if (inputFieldEl.value === "" || inputFromEl.value === "" || inputToEl.value === "") {
     alert("INVALID! YOU MUST COMPLETE THE FORM!");
@@ -81,6 +81,9 @@ function appendItemToShoppingListEl(item) {
   toText = "<strong>To:</strong> " + toText.substr(3);
   fromText = "<strong>From:</strong> " + fromText.trim();
 
+
+  
+
   // create a new HTML element to display the itemValue
   let newItemValue = document.createElement("div");
   newItemValue.innerHTML = toText + "<br>" + fromText;
@@ -92,7 +95,7 @@ function appendItemToShoppingListEl(item) {
   newLikeCount.textContent = parseInt(localStorage.getItem(itemID)) || 0;
   newLikeButton.addEventListener("click", function () {
     let currentCount = parseInt(newLikeCount.textContent) || 0;
-    currentCount++;
+   
     newLikeCount.textContent = currentCount;
     localStorage.setItem(itemID, currentCount);
     newEl.removeEventListener("click", function () {});
@@ -104,18 +107,32 @@ function appendItemToShoppingListEl(item) {
    newLikeButton.addEventListener("click", function () {
     newLikeButton.disabled = true;
   });
-
+  let liked = localStorage.getItem(itemID + '_liked');
+  if (liked) {
+    newLikeButton.disabled = true;
+  }
+  
+  newLikeButton.addEventListener('click', function () {
+    let currentCount = parseInt(newLikeCount.textContent) || 0;
+    currentCount++;
+    newLikeCount.textContent = currentCount;
+    localStorage.setItem(itemID, currentCount);
+    localStorage.setItem(itemID + '_liked', true);
+    newLikeButton.disabled = true;
+  });
+  
   newDiv.appendChild(newLikeButton);
   newDiv.appendChild(newLikeCount);
   newEl.appendChild(newDiv);
 
-  //newEl.addEventListener("dblclick", function () {
-   // let exactLocationOfItemInDB = ref(database, `endoresement/${itemID}`)
-   // remove(exactLocationOfItemInDB)
-  //});
+  newEl.addEventListener("dblclick", function () {
+    let exactLocationOfItemInDB = ref(database, `endoresement/${itemID}`)
+   remove(exactLocationOfItemInDB)
+  });
 
   shoppingListEl.append(newEl);
 }
+
 
 
 // Load the like count from local storage or set to 0 if it doesn't exist
@@ -145,4 +162,5 @@ ulList.addEventListener('DOMNodeInserted', function (event) {
   // set the new height for the mainDiv
   mainDiv.style.height = height + 'px';
 });
+
 
